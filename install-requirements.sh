5760483c378be7409c8261fd82b376e7b1bde265
Mateusz Lewczak <leftarcode@gmail.com>
2023-10-02 21:30:37 +0200

    Initial commit

---
+++ install-requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "[!] Please run as root"
+  exit
+fi
+
+apt -y update
+apt -y install git python3 python3-distutils uuid-dev build-essential bison flex nasm
